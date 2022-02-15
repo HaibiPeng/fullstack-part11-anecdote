47734d27c89e4fef15df28c080713be3835756cc
HaibiPeng <haibi.peng@aalto.fi>
2022-02-15 16:57:03 +0200

    ex21

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const anecdotes = require('./db.json')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'build')))
 
@@ -11,12 +11,12 @@ app.get('/health', (req, res) => res.send('ok'))
 app.get('/version', (req, res) => res.send('V1.1'))
 
 app.get('/anecdotes', (req, res) => {
-  res.send(anecdotes) // change this string to ensure a new version deployed
+  res.send(anecdotes)
 })
 
 app.post('/anecdotes', (req, res) => {
   try {
-    const newAnecdote = { ...req.body, id: uuid.v1() }
+    const newAnecdote = { ...req.body, id: req.body.content }
     anecdotes.push(newAnecdote)
     res.send(newAnecdote)
   } catch (error) {
@@ -24,19 +24,7 @@ app.post('/anecdotes', (req, res) => {
   }
 })
 
-app.put('/anecdotes/:id', (req, res) => {
-  try {
-    const idx = anecdotes.findIndex((obj => obj.id === req.params.id))
-    anecdotes[idx].votes += 1
-    res.send(anecdotes[idx])
-  } catch (error) {
-    res.status(400).send(error.message)
-  }
-})
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.use(express.static('build'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
